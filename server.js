178fd669cd33af28ee5c6a307eeb8e97eb882784
PalOfCode <rickpal005@gmail.com>
2026-09-07 06:29:07 +0530

    Allow Vercel frontend origin

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(
       "http://localhost:5173",
       "http://127.0.0.1:5173",
       "http://172.20.10.2:5173",
+       "https://maap-setu-two.vercel.app",
     ],
     credentials: true,
   })
